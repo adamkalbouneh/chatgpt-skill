b7ac9c8ed93b24bc307bd7743b78eab4dd4272c1
adamkalbouneh <125579403+adamkalbouneh@users.noreply.github.com>
2023-03-21 15:45:09 +0000

    Marwa

--- __init__.py
+++ __init__.py
@@ -1,15 +1,22 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import openai
+import os
 
 class Chatgpt(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        openai.api_key = os.environ["OPENAI_API_KEY"]  # Set the API key
 
     @intent_file_handler('chatgpt.intent')
     def handle_chatgpt(self, message):
-        self.speak_dialog('chatgpt')
-
+        prompt = "Hello, how are you?"  # Example prompt
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=60
+        )
+        text = response.choices[0].text.strip()  # Get the generated text
+        self.speak(text)  # Speak the generated text
 
 def create_skill():
     return Chatgpt()
-
